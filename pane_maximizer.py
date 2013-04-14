eed1ba71562cc7594aa7ef35621ec16f544576c6
JD Isaacks <jd@jisaacks.com>
2013-04-14 18:08:18 -0400

    Tidy up

--- pane_maximizer.py
+++ pane_maximizer.py
@@ -10,7 +10,6 @@ class PaneManager:
 
 class PaneMaximizerCommand(sublime_plugin.WindowCommand):
     def run(self):
-        print("PMC")
         w = self.window
         if w.num_groups() > 1:
             w.run_command("maximize_pane")
@@ -21,11 +20,9 @@ class PaneMaximizerCommand(sublime_plugin.WindowCommand):
 
 class MaximizePaneCommand(sublime_plugin.WindowCommand):
     def run(self):
-        print("MAX")
         w = self.window
 
         if w.num_groups() == 1:
-            print("exiting")
             return False
             exit()
 
@@ -38,6 +35,7 @@ class MaximizePaneCommand(sublime_plugin.WindowCommand):
         }
 
         PaneManager.view_positions = []
+        
         for view in w.views():
             group, index = w.get_view_index(view)
             PaneManager.view_positions.append([view, group, index])
@@ -48,16 +46,13 @@ class MaximizePaneCommand(sublime_plugin.WindowCommand):
 
 class UnmaximizePaneCommand(sublime_plugin.WindowCommand):
     def run(self):
-        print("MIN")
         w = self.window
 
         last_layout = PaneManager.last_layout
         PaneManager.last_layout = False
         active_view = w.active_view()
+        
         if last_layout:
-            # print(PaneManager.view_positions)
-
-            # for group, indexes in PaneManager.views_by_groups
 
             w.set_layout(last_layout)
 
