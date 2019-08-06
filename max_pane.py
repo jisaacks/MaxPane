881a944fe115f77f1dfc6206634e6e064aaf81c8
DeathAxe <deathaxe@googlemail.com>
2019-08-06 16:46:59 +0200

    Merge layouts_doc into max_pane
    The layouts_doc is loaded as plugin, which is useless as it does not
    contain any text. Therefore its content is added as docstring to the
    max_pane module.

--- max_pane.py
+++ max_pane.py
@@ -1,3 +1,41 @@
+"""
+  DESCRIPTION ABOUT SUBLIME TEXT LAYOUTS:
+
+  window.get_layout() and window.set_layout() aren't really documented
+  in the API so I am making some notes here about how they work.
+
+    * rows are the positions (from 0.0 to 1.0) that make
+      up the horizontal lines around (sides of) views.
+
+    * columns are the positions (from 0.0 to 1.0) that make
+      up the vertical lines around (top/bottom of) views
+
+    * cells are the 4 positions (left,top,right,bottom)
+      that define the positions of each view
+
+  ROWS:
+
+  0.0        -----------------
+             |    |          |
+             |    |          |
+  0.66       -----------------
+             |    |          |
+  1.0        -----------------
+
+  COLUMNS:  0.0  0.3        1.0
+
+  CELLS:
+
+    ---------
+    | 0 | 0 |
+    |0 1|1 2|
+    | 1 | 1 |
+    ---------
+    | 1 | 1 |
+    |0 1|1 2|
+    | 2 | 2 |
+    ---------
+"""
 import sublime
 import sublime_plugin
 
@@ -5,6 +43,7 @@ import sublime_plugin
 SHARE_OBJECT = 'max_pane_share.sublime-settings'
 
 # ------
+
 
 class ShareManager:
     """Exposes a list of window ids which currently contain maximized panes.
